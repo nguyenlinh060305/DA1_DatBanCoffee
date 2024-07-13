9295177691c0d4cd0877af2dab36739ec197dcda
sang205 <sanghoang12012005@gmail.com>
2024-07-13 19:48:48 +0700

    13/07/2014 Form

---
+++ JavaApplication1/src/model/BanHang_Ban.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sangh
+ */
+public class BanHang_Ban {
+
+    private String viTri;
+    private String tenBan;
+    private String trangThai;
+
+    public BanHang_Ban() {
+    }
+
+    public BanHang_Ban(String viTri, String tenBan, String trangThai) {
+        this.viTri = viTri;
+        this.tenBan = tenBan;
+        this.trangThai = trangThai;
+    }
+
+    public String getViTri() {
+        return viTri;
+    }
+
+    public void setViTri(String viTri) {
+        this.viTri = viTri;
+    }
+
+    public String getTenBan() {
+        return tenBan;
+    }
+
+    public void setTenBan(String tenBan) {
+        this.tenBan = tenBan;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+}
